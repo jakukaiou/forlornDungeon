f4008367d133f48e83ad80c2a4cbb6f2fc88775b
tekkyuu <byabyabya.dx@gmail.com>
2017-01-24 03:46:38 +0900

    create title

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# Typescript-Phaser-environment
-phaser environment webpack + webpack-dev-server + typescript
-
-## usage
-1. npm install -g webpack-dev-server
-1. npm install
-1. acccess http://localhost:8080/webpack-dev-server/ (or displayed address)
-1. edit src/ts/game.ts
+# forlornDungeon
+(Later)
--- src/ts/game.ts
+++ src/ts/game.ts
@@ -1,8 +1,10 @@
+//USE Web font
 /// <reference path="../../node_modules/phaser/typescript/phaser.d.ts" />
 declare function require(x: string): any;
 var globalPhaser = require('phaser');
 
 class SimpleGame {
+    title:Phaser.Text;
 
     constructor() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
@@ -11,12 +13,13 @@ class SimpleGame {
     game: Phaser.Game;
 
     preload() {
-       this.game.load.image('logo', 'assets/images/phaser2.png');
+       //this.game.load.image('logo', 'assets/images/phaser2.png');
     }
 
     create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        //var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        //logo.anchor.setTo(0.5, 0.5);
+        this.title = this.game.add.text(100,100,"Forlorn Dungeon!!",{font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle"});
     }
 
 }
